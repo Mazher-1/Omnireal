abe2fdd1455638fb2045d7ade6df50c89bab39fb
MH Code <mazheriftikhar1@gmail.com>
2024-10-14 14:42:14 +0500

    14-oct changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <div>
-      <h1 className='text-blue-500'>
-        Hello world
-      </h1>
+       <Navbar />
     </div>
   )
 }
